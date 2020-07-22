654702e91fea50052ec7cc79b214237ab2b21053
Jessica Wolff <jleahwolff@gmail.com>
2020-07-22 12:51:36 -0500

    Done with day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,33 +2,50 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.sp = 255
+        self.is_running = True
 
-    def load(self):
+
+    def load(self, p):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+    
+        with open(p) as program:
+            for line in program:
+                instruction = line.split('#')[0].strip()
+                if instruction != '':
+                    try:
+                        self.ram[address] = int(instruction, 2)
+                        address += 1
+                    except IndexError:
+                        print('Memory overflow!')
+                        return
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +79,47 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.is_running:
+            # read the ram at that address and find if it is a command or value
+            instruction = self.ram_read(self.pc)
+            if instruction == 0b10000010:  # LDI
+                reg_address = self.ram_read(self.pc + 1)
+                reg_value = self.ram_read(self.pc + 2)
+                self.reg[reg_address] = reg_value
+                self.pc += 3
+            elif instruction == 0b10100010:  # MULT
+                # store values in regA and regB
+                regA = self.ram_read(self.pc + 1)
+                regB = self.ram_read(self.pc + 2)
+                # store product in regA
+                self.reg[regA] = self.reg[regA] * self.reg[regB]
+                self.pc += 3
+            elif instruction == 0b01000101:  # PUSH
+                # push to R7
+                self.sp -= 1
+                mdr = self.reg[self.ram_read(self.pc + 1)]
+                self.ram_write(self.sp, mdr)
+                self.pc += 2
+            elif instruction == 0b01000110:  # POP
+                # pop to R8
+                regA = self.ram_read(self.pc + 1)
+                mdr = self.ram_read(self.sp)
+                self.reg[regA] = mdr
+                self.pc += 2
+                self.sp += 1
+            elif instruction == 0b01000111:  # PRN
+                print(int(self.reg[self.ram_read(self.pc + 1)]))
+                self.pc += 2
+            elif instruction == 0b00000001:  # HLT
+                self.is_running = False
+            else:
+                self.pc += 1
+
+            if self.pc > 255:
+                self.is_running = False
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mdr, value):
+        self.ram[mdr] = value
